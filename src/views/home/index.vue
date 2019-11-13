0db1cb04643b72088e5382284727ea80f6a52b10
guohaoning1996 <1335806944@qq.com>
2019-11-13 16:17:36 +0800

    初始化

---
+++ src/views/home/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class='container'>
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'></style>
